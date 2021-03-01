492c3cf7a2dc94b56001cf7bd0aac60aadbc09e5
willemrood <willemrood@gmail.com>
2021-03-01 13:23:03 +0100

    solved 18 to get into the 25 club

--- problem18.py
+++ problem18.py
@@ -16,5 +16,25 @@ data = [[75],
 [91, 71, 52, 38, 17, 14, 91, 43 ,58 ,50 ,27, 29, 48],
 [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
 [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38,53, 60, 4, 23]]
+choices = 14
+combos = 2**choices
+best = 0
 
-for x in itt.combinations( xrange(n), n1 )
+for i in range(combos):
+    bin_string = bin(i)[2:]
+    for zero in range(choices-len(bin_string)):
+        bin_string = "0" + bin_string
+    sum = 75
+    index_mem = 0
+    row_mem = 1
+    history = []
+    for j in bin_string:
+        sum += data[row_mem][index_mem+int(j)]
+        history.append(data[row_mem][index_mem+int(j)])
+        index_mem += int(j)
+        row_mem += 1
+    if sum>best:
+        best=sum
+        best_history = history
+print(best_history)
+print(best)
